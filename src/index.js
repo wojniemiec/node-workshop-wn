bb81fb7d0b39a250f3fc01dc0be34c5ae2c47608
Wojciech Niemiec <wojciech.niemiec@schibsted.pl>
2016-01-26 13:24:21 +0100

    stupid commit

--- src/index.js
+++ src/index.js
@@ -5,6 +5,6 @@ var stockRepo = require('./repository/stock-repository');
 var app = require('./app')(stockRepo);
 var port = process.env.PORT ? process.env.PORT : 3000;
 app.listen(port, function () {
-  console.log('Example app listening on port 3000!');
+  console.log('Example app listening on port 3000! Happy coding');
 });
 
